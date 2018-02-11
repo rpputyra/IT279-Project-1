544d39ccf7972ff80814041929a1caadae40ce5b
Runyan <caruny1@maple.ad.ilstu.edu>
2018-02-11 14:27:40 -0600

    Consolidated files into src folder

---
+++ src/Stack.h
@@ -0,0 +1,17 @@
+/*
+ * Stack.h
+ *
+ *  Created on: Feb 4, 2018
+ *      Author: rpput
+ */
+
+#ifndef STACK_H_
+#define STACK_H_
+
+class Stack {
+public:
+	Stack();
+	virtual ~Stack();
+};
+
+#endif /* STACK_H_ */
