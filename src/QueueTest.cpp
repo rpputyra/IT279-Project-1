44d41accbb24f7028e0217b8a160b6d05acc33a2
Runyan <caruny1@maple.ad.ilstu.edu>
2018-02-12 14:55:50 -0600

    Placed initial data inside of Queue.cpp and QueueTest.cpp

--- src/QueueTest.cpp
+++ src/QueueTest.cpp
@@ -1,1 +1,16 @@
- 
+ /*
+ * Assignment 1: Stacks & Queues
+ * QueueTest.cpp
+ * Created: 02/12/2018
+ * Author: Christopher A Runyan
+ */
+#include <iostream>
+#include "Stack.cpp"
+
+using namespace std;
+
+int main(){
+   //test queue 
+
+   return 0;
+}
