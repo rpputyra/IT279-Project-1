7cefe7d5a2d1e16902e9e347bbf004e2c7952910
Runyan <caruny1@pine.ad.ilstu.edu>
2018-02-14 17:56:47 -0600

    Queue.cpp and CueueTest.cpp completed - code needs cleaned

--- src/Queue.cpp
+++ src/Queue.cpp
@@ -10,7 +10,123 @@ using namespace std;
 
 class Queue{
    struct Node{
+      public:int data;
+      public:Node* next;
 
+      public:Node(int data){
+         this->data=data;
+      }
+
+      public:Node(int data, Node* next){
+         this->data=data;
+         this->next=next;
+      }
+
+      public:int getData(){
+         return data;
+      }
+
+      public:void print(){
+         cout<<"Data: "<<data<<"\n\n";
+      }
+
+      public:bool equals(Node* toTestEquals){
+         if(toTestEquals->data==data){
+            return true;
+         }
+         else{
+            return false;
+         }
+      }
    };
 
+private:
+   Node* frontNode;
+   int size;
+
+public:
+   //constructor
+   Queue(){
+      frontNode = NULL;
+      size = 0;
+   }
+
+   //destructor
+   ~Queue(){
+      Node* next = frontNode;
+      
+      while(next){
+         Node *deleteMe = next;
+         next = next->next;
+         delete deleteMe;
+      }
+   }
+
+   void push(int data){
+      Node* temp = frontNode;
+
+      if(size == 0){
+         frontNode = new Node(data);
+      }
+      else{
+         for(int i=0; i<size-1; i++){
+            temp = temp->next;
+         }
+         temp->next = new Node(data);
+      }
+
+      size++;
+   }
+
+   int front(){
+      return frontNode->getData();
+   }
+
+   int back(){
+      Node* toReturn = frontNode;
+      int data;
+      int i=0;
+
+      while(i<size){
+         toReturn = toReturn->next;
+         i++;
+      }
+      data = toReturn->getData();
+      delete toReturn;
+
+      return data;
+   }
+
+   int pop(){
+      if(size > 0){
+         Node* frontNodeTemp = frontNode;
+         frontNode = frontNode->next;
+         size--;
+      
+         return frontNodeTemp->getData();
+      }
+      else{
+         return 0;
+      }
+   }
+ 
+   bool empty(){
+      if(size == 0){
+         return true;
+      }
+      else{
+         return false;
+      }
+   }
+
+   void print(){
+      Node* toPrint = frontNode;
+      int i=0;
+
+      while(i<size){
+         toPrint->print();
+         toPrint = toPrint->next;
+         i++;
+      }
+   }
 };
--- src/QueueTest.cpp
+++ src/QueueTest.cpp
@@ -5,12 +5,45 @@
  * Author: Christopher A Runyan
  */
 #include <iostream>
-#include "Stack.cpp"
+#include "Queue.cpp"
 
 using namespace std;
 
 int main(){
-   //test queue 
+   Queue* queue=new Queue();
+   cout<<"New queue created.\n";
+
+   cout<<"About to test push method.\n";
+   queue->push(1);
+   cout<<"Push method tested OK.\n";
+   cout<<"Queue after push method:\n"; 
+   queue->print();
+
+   queue->pop();
+   cout<<"\nPop method tested OK.\n";
+   cout<<"Queue after pop method:\n\n";
+   queue->print();
+
+   queue->pop();
+   cout<<"\nPop method tested OK.\n";
+   cout<<"Queue after pop method:\n\n";
+   queue->print();
+
+   queue->push(1);
+   queue->push(3);
+   queue->push(5);
+   cout<<"\nQueue after 3 push methods:\n"; 
+   queue->print();
+
+   queue->pop();
+   cout<<"\nQueue after pop method:\n";
+   queue->print();
+
+   queue->push(7);
+   cout<<"\nQueue after adding:\n";
+   queue->print();
+   
+   cout<<"Node at front: "<<queue->front()<<"\n";
 
    return 0;
 }
